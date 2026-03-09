ec23d55ae3419802002c77be3689b654d410651f
Kanishka <kanishkashemalperera@gmail.com>
2026-03-09 20:53:17 +0530

    change main app content 4

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     return (
         <div className="app-container">
             <header className="app-header">
-                <h1>Welcome Kanishka 👋</h1>
+                <h1>Welcome pamuditha modaya 👋</h1>
                 <p>This is my first React frontend page</p>
                 <button className="btn">Click Me</button>
             </header>
